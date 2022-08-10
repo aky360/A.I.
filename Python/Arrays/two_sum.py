55810378a0fc72de866865ab6c1c163ea427db29
aky360 <95171943+aky360@users.noreply.github.com>
2022-08-10 23:44:14 +0530

    Delete Python/Arrays directory

--- Python/Arrays/two_sum.py
+++
@@ -1,8 +0,0 @@
-def two_sum(array, target):
-    dic = {}
-    for i, num in enumerate(array):
-        if num in dic:
-            return dic[num], i
-        else:
-            dic[target - num] = i
-    return None
