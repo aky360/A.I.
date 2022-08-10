21f3c04cc223ea1d96e9dec28e3173f25fd3a54d
aky360 <95171943+aky360@users.noreply.github.com>
2022-08-11 00:56:13 +0530

    Update diagonalDifference.py

--- Python/Matrix/diagonalDifference.py
+++ Python/Matrix/diagonalDifference.py
@@ -1,8 +1,8 @@
 
 def diagonalDifference(arr):
-    # Write your code here
     x, y = 0, 0
     n = len(arr)
+    
     for i in range(0, len(arr)):
         for j in range(0, len(arr[0])):
             if(i == j):
@@ -14,10 +14,11 @@ def diagonalDifference(arr):
     return abs(x-y)
 
 
-print("row ", len(arr))
-print("column ",len(arr[0]))
+
 
 arr = [[11, 2, 4],[4, 5, 6],[10, 8, -12]]
+print("row ", len(arr))
+print("column ",len(arr[0]))
 
 print(diagonalDifference(arr))
   
