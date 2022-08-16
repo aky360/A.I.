07cb610c342667be39071a0895fe9970841ebe8a
aky360 <95171943+aky360@users.noreply.github.com>
2022-08-16 13:20:48 +0530

    Create addTwoNum.py

---
+++ Python/Basics/addTwoNum.py
@@ -0,0 +1,42 @@
+"""
+Add two numbers without using arithmetic operators (+, -)
+"""
+
+class Add:
+    
+    def addTwo(self, x, y):
+        for i in range(1, y+1):
+            x += 1
+            
+        return x
+        
+        
+    def addTwoItr(self, x, y):
+        # Iterate till there is no carry
+        while (y != 0):
+        # carry now contains common
+        # set bits of x and y
+            carry = x & y
+        # Sum of bits of x and y where at
+        # least one of the bits is not set
+            x = x ^ y
+        # Carry is shifted by one so that  
+        # adding it to x gives the required sum
+            y = carry << 1
+            # print(carry, ' ', x, ' ', y)
+        return x
+    
+    
+    def addTwoRec(self, x, y):
+        if(y==0):
+            return x
+            
+        return self.addTwoRec(x^y, (x&y)<<1)
+    
+        
+        
+add = Add()
+    
+print(add.addTwo(1, 23))
+print(add.addTwoItr(1, 23))
+print(add.addTwoRec(1, 23))
