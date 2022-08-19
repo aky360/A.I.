e94a92f09525ba85b374f78d936d7d267c2bf1f6
aky360 <95171943+aky360@users.noreply.github.com>
2022-08-19 15:57:44 +0530

    Create reverse_S_L_List.py

---
+++ Python/Linked_List/reverse_S_L_List.py
@@ -0,0 +1,57 @@
+class SLinkedList:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+    
+root = SLinkedList(1)
+root1 = SLinkedList(2)
+root2 = SLinkedList(3)
+root3 = SLinkedList(4)
+root4 = SLinkedList(5)
+root.next = root1
+root1.next = root2
+root2.next = root3
+root3.next = root4
+root4.next = None
+
+
+"""another way to creating the linked list is"""
+
+head = SLinkedList(5)
+head.next = SLinkedList(6)
+head.next.next = SLinkedList(7)
+head.next.next.next = SLinkedList(8)
+    
+def printLList(head):
+    if not head or not head.next:
+        return head
+    else:
+        while head!=None:
+            print('data ' ,head.data)
+            head = head.next
+    
+"""
+Reverse a singly linked list. For example:
+1 --> 2 --> 3 --> 4
+After reverse:
+4 --> 3 --> 2 --> 1
+"""
+
+def rev_LList(head):
+    if not head or not head.next:
+        return head
+        
+    prev = None
+    while head:
+        curr = head
+        head = head.next
+        curr.next = prev
+        prev = curr
+    return prev    
+        
+print('before reverse data ')
+print(printLList(head))
+rev = rev_LList(head)
+print('after reverse list data ')
+print(printLList(rev))
