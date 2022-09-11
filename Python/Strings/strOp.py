e0b1f029d68077a65d8a76abc4f7a272ae8877b2
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-11 22:51:04 +0530

    Update strOp.py

--- Python/Strings/strOp.py
+++ Python/Strings/strOp.py
@@ -1,27 +1,28 @@
 string = "PrOgRaMMing is the fun "
 
-print(string.lower())
-print(string.upper())
+print(string.lower())                                          # programming is the fun 
+print(string.upper())                                          # PROGRAMMING IS THE FUN
 
 
 string = "This will split all words into a list"
 
-print(string.split())
+print(string.split())                                           # ['This', 'will', 'split', 'all', 'words', 'into', 'a', 'list']
 
 string = "This will split all words into a list"
 list_a = string.split()
-print(list_a)
+print(list_a)                                                   # ['This', 'will', 'split', 'all', 'words', 'into', 'a', 'list']
 
-x = ' '.join(list_a)
-y = ', '.join(list_a)
-z = ' :'.join(list_a)
+x = ' '.join(list_a)                                            # 
+y = ', '.join(list_a)                                           #
+z = ' :'.join(list_a)                                           #
 
-print(x)
-print(y)
-print(z)
+print(x)                                                        # This will split all words into a list
+print(y)                                                        # This, will, split, all, words, into, a, list
+print(z)                                                        # This :will :split :all :words :into :a :list
 
 
-string = 'Happy New Year'
-print(string.find('ew'))
+string = 'Happy New Year'          
+print(string)                                                   # Happy New Year
+print(string.find('ew'))                                        # 7
 
-print(string.replace('Happy','Brilliant'))
+print(string.replace('Happy','Brilliant'))                      # Brilliant New Year
