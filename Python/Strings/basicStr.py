3b5fa78712a64fd526f01bfeb35820249c8d171e
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-11 22:35:56 +0530

    Update basicStr.py

--- Python/Strings/basicStr.py
+++ Python/Strings/basicStr.py
@@ -1,3 +1,58 @@
 print("Binary representation of {0} is {0:b}".format(12))
 
 print("{0:b}".format(10))
+
+
+
+str = 'cold'
+
+# enumerate()
+list_enumerate = list(enumerate(str))
+print('list(enumerate(str) = ', list_enumerate)           # list(enumerate(str) =  [(0, 'c'), (1, 'o'), (2, 'l'), (3, 'd')]
+
+#character count
+print('len(str) = ', len(str))                            # len(str) =  4
+
+
+
+# Python string format() method
+# default(implicit) order
+default_order = "{}, {} and {}".format('John','Bill','Sean')
+print('\n--- Default Order ---')
+print(default_order)                                                     # --- Default Order --- 
+                                                                         # John, Bill and Sean
+
+# order using positional argument
+positional_order = "{1}, {0} and {2}".format('John','Bill','Sean')
+print('\n--- Positional Order ---')
+print(positional_order)                                                  # --- Positional Order ---
+                                                                         # Bill, John and Sean
+
+# order using keyword argument
+keyword_order = "{s}, {b} and {j}".format(j='John',b='Bill',s='Sean')
+print('\n--- Keyword Order ---')
+print(keyword_order)                                                     # --- Keyword Order ---
+                                                                         # Sean, Bill and John
+  
+
+# formatting integers
+>>> "Binary representation of {0} is {0:b}".format(12)
+'Binary representation of 12 is 1100'
+
+>>> # formatting floats
+>>> "Exponent representation: {0:e}".format(1566.345)
+'Exponent representation: 1.566345e+03'
+
+>>> # round off
+>>> "One third is: {0:.3f}".format(1/3)
+'One third is: 0.333'
+
+>>> # string alignment
+>>> "|{:<10}|{:^10}|{:>10}|".format('butter','bread','ham')
+'|butter    |  bread   |       ham|'
+
+>>> x = 12.3456789
+>>> print('The value of x is %3.2f' %x)
+The value of x is 12.35
+>>> print('The value of x is %3.4f' %x)
+The value of x is 12.3457
