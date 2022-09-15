18a13ceb59373d6f67715b2e98d8cc8ac13708c7
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-15 14:44:50 +0530

    Create findSquareRoot.py

---
+++ Python/Basics/findSquareRoot.py
@@ -0,0 +1,16 @@
+def squareRoot(x: int)->int:
+    left=1
+    right=x
+    result=0
+    while(left<right):
+        mid = int(left + (right-left)/2)
+        if(mid<x/mid):
+            left = mid+1
+        else:
+            right = mid
+        
+    return left if(left == x/left) else left-1
+    
+print(squareRoot(4096))
+print(squareRoot(1048))
+print(squareRoot(121))
