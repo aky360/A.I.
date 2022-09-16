839cf5c555e350286d10079c3f4ecec63da09acd
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-16 13:11:28 +0530

    Update findSquareRoot.py

--- Python/Basics/findSquareRoot.py
+++ Python/Basics/findSquareRoot.py
@@ -8,6 +8,23 @@ def squareRoot(x: int)->int:
             right = mid
         
     return left if(left == x/left) else left-1
+
+
+#ANOTHER WAY TO FIND THE SQUAREROOT OF ANY NUMBER 
+def mySqrt(x: int) -> int:
+        left, right=0,x
+        while left<=right:
+            mid=left+ (right-left)//2
+            if mid*mid >x:
+                right = mid-1
+            elif mid*mid < x:
+                left =mid+1
+            else:
+                return mid
+        return right
+
+print(mySqrt(100))
+
     
     
 if __name__ == "__main__" :
