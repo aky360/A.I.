0989d7506ddf46050cb00ebe02e4a0d2e779f43d
aky360 <95171943+aky360@users.noreply.github.com>
2022-09-25 16:55:38 +0530

    Update basicPrac.py

--- Python/Basics/basicPrac.py
+++ Python/Basics/basicPrac.py
@@ -1,29 +1,29 @@
 arr = [1,2,334,4,5]
 
-print(type({*arr}))
-print(*arr)
+print(type({*arr}))               # <class 'set'>
+print(*arr)                       # 1 2 334 4 5
 
-print(type([*arr]))
-print(*arr)
+print(type([*arr]))                # <class 'list'>
+print(*arr)                        # 1 2 334 4 5
 
-print(type((*arr,)))
-print(*arr)
+print(type((*arr,)))               # <class 'tuple'>
+print(*arr)                        # 1 2 334 4 5
 
 dic = {}
 for i, num in enumerate(arr):
-    print(i,"=>",num)
+    print(i,"=>",num)                    # 0 => 1, 1 => 2, 2 => 334, 3 => 4, 4 => 5
     dic[i] = num
     
     
-print([*dic.values()])
-print([*dic.keys()])
+print([*dic.values()])                           # [1, 2, 334, 4, 5]
+print([*dic.keys()])                             # [0, 1, 2, 3, 4]
 
-print((*dic.values(),))
-print((*dic.keys(),))
+print((*dic.values(),))                          # (1, 2, 334, 4, 5)
+print((*dic.keys(),))                            # (0, 1, 2, 3, 4)
 
-print({*dic.values()})
-print({"2" : {*dic.keys()}})
+print({*dic.values()})                           # {1, 2, 4, 5, 334}
+print({"2" : {*dic.keys()}})                     # {'2': {0, 1, 2, 3, 4}}
     
 tp ={'1' : [*arr]}
-print(type(tp))
-print(*arr)
+print(type(tp))                                  # <class 'dict'>
+print(*arr)                                      # 1 2 334 4 5
