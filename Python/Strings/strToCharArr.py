e595fc657230dd4dd851306420f7f91a813d3beb
aky360 <95171943+aky360@users.noreply.github.com>
2022-10-30 16:52:22 +0530

    Update strToCharArr.py

--- Python/Strings/strToCharArr.py
+++ Python/Strings/strToCharArr.py
@@ -20,3 +20,19 @@ print(to_array)                             # ['s', 't', 'u', 'd', 'y', 't', 'o'
 string = "studytonight"
 to_array = [*string]
 print(to_array)                             # ['s', 't', 'u', 'd', 'y', 't', 'o', 'n', 'i', 'g', 'h', 't']
+
+
+
+string = 'geeksforgeeks'
+lst = []
+for letter in string:
+    lst.append(letter)
+print(lst)                                  # ['g', 'e', 'e', 'k', 's', 'f', 'o', 'r', 'g', 'e', 'e', 'k', 's']
+
+
+
+def split(word):
+    return list(word)
+
+word = 'geeks'
+print(split(word))                          # ['g', 'e', 'e', 'k', 's']
