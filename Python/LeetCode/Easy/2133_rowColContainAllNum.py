3878dd99ec22cfbeaf23c5921ff3ae1558253bd5
aky360 <95171943+aky360@users.noreply.github.com>
2022-10-30 17:52:22 +0530

    Update 2133_rowColContainAllNum.py

--- Python/LeetCode/Easy/2133_rowColContainAllNum.py
+++ Python/LeetCode/Easy/2133_rowColContainAllNum.py
@@ -1,3 +1,3 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
-        return all(min(len(set(row)), len(set(col))) == len(matrix) for row, col in zip(matrix, zip(*matrix)))
+        return all(min(len(set(row)), len(set(col))) == len(matrix) for row, col in zip(matrix, zip(*matrix)))   # all() checks all data in all() function are equal or not return T/F
