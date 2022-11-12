169982c47c28e078c4d7251616845c6252c75a5e
aky360 <95171943+aky360@users.noreply.github.com>
2022-11-12 15:04:44 +0530

    Create revArr.py

---
+++ Python/Arrays/revArr.py
@@ -0,0 +1,27 @@
+# Iterative way.
+def revArray(A, start, end):
+	while start < end:
+		A[start], A[end] = A[end], A[start]
+		start += 1
+		end -= 1
+
+A = [1,2,3,4,5,6,7]
+revArray(A, 0, 6)
+print("reversed list is :", A)
+
+
+
+# Recursive python program to reverse an array
+ 
+# Function to reverse A[] from start to end
+def reverseList(A, start, end):
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    reverseList(A, start+1, end-1)
+ 
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+reverseList(A, 0, 5)
+print("Reversed list is :", A)
+
