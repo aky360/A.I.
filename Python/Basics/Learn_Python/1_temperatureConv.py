2d44f32f575076a832c52f325dc5eca27a315d06
aky360 <95171943+aky360@users.noreply.github.com>
2023-08-03 12:11:47 +0530

    Create 1_temperatureConv.py

---
+++ Python/Basics/Learn_Python/1_temperatureConv.py
@@ -0,0 +1,5 @@
+c = int(input("enter temperature in celsius "))
+f = int(input("enter temperature in fahrenheit "))
+
+print("temperature celsius to fahrenheit ", (c*1.8)+32) 
+print("temperature fahrenheit to celsius ",(f - 32) * 5/9)
