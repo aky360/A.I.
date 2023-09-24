416e91360ba11a6fb72469952eb24c79ff55bb49
aky360 <95171943+aky360@users.noreply.github.com>
2023-09-24 14:00:00 +0530

    Create pyTemp_24-09-23.py

---
+++ Python/Basics/pyTemp_24-09-23.py
@@ -0,0 +1,37 @@
+a = '1301'
+bins = ['0', '1']
+
+
+print(' a b '[::-1])
+
+num = 234
+num //= 10
+print(num)
+
+x = 200
+print(x%10)
+
+for i in a:
+    if i not in bins:
+        print("NO")
+        
+        
+def reverse_digit(n):
+    # Code here
+    rev, num = 0, n
+    while(num<0):
+        remainder = num%10
+        rev = rev*10 + remainder
+        num //= 10
+        print(rev)
+		
+    return rev
+    
+print()
+print(reverse_digit(2234))
+        
+def isBinary(str):
+    return str.count('0') + str.count('1') == len(str)
+    
+    
+print(isBinary(a))
