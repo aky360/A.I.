b7a91904adff187a2850ab95eb7c4b5159d8ac47
aky360 <95171943+aky360@users.noreply.github.com>
2023-10-09 13:09:48 +0530

    Update revAlphabet.py

--- Python/Strings/revAlphabet.py
+++ Python/Strings/revAlphabet.py
@@ -28,16 +28,33 @@ class Solution:
         for i in range(26):
             dicL[chr(65+i)] = chr(90-i)
             dicS[chr(97+i)] = chr(122-i)
-            
         for val in S:
             if val!=' ':
-                if val in dicS.keys():
-                    ans += dicS.get(val)
-                if val in dicL.keys():
-                    ans += dicL.get(val)
-            else:
-                ans += " "
-        
+                if val in dicS.keys(): ans += dicS.get(val)
+                if val in dicL.keys(): ans += dicL.get(val)
+            else: ans += " "
+        return ans
+
+class Solution:
+    def reciprocalString(self, S):
+        dicS, dicL, ans = {}, {}, ""
+        for i in range(26):
+            dicL[chr(65+i)] = chr(90-i)
+            dicS[chr(97+i)] = chr(122-i)
+        for val in S:
+            if val in dicS.keys(): ans += dicS.get(val)
+            elif val in dicL.keys(): ans += dicL.get(val)
+            else: ans += val
+        return ans
+
+
+class Solution:
+    def reciprocalString(self, S):
+        ans=""
+        for x in S:
+            if x>='A' and x<='Z': ans+=chr(90-(ord(x)-65))
+            elif x>='a' and x<='z': ans+=chr(122-(ord(x)-97))
+            else: ans+=x
         return ans
 
 
