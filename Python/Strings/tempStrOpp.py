272077f7484c28a4e391697e6efbf61a4d28826c
aky360 <95171943+aky360@users.noreply.github.com>
2023-10-24 18:10:49 +0530

    Update tempStrOpp.py

--- Python/Strings/tempStrOpp.py
+++ Python/Strings/tempStrOpp.py
@@ -37,3 +37,54 @@ ax.sort(key=lambda a:a[0])
 print(ax)                                                      #[('a', 5), ('geek', 50), ('ram', 10), ('xbnnskd', 100)]
 ax.sort(key=lambda a:a[1])
 print(ax)                                                      #[('a', 5), ('ram', 10), ('geek', 50), ('xbnnskd', 100)]
+
+
+Strs = 'aeiouAEIOU'
+ll = list('aeiouAEIOU')
+print(ll)
+strList = Strs.split()
+print(strList)
+
+atx = list(Strs)
+print(atx)
+atx.remove(atx[0])
+print(atx)
+print(''.join(atx))
+
+sm = "small"
+vowels=['a','e','i','o','u','y']
+print(vowels[::])
+ss=list(sm)
+li=ss[::]
+print()
+print(ss[::])
+print(li)
+
+
+def Sandwiched_Vowel(s):
+    vowels=['a','e','i','o','u']
+    s=list(s)
+    li=s[::]
+    for i in range(1,len(s)-1):
+        if s[i-1] not in vowels and s[i+1] not in vowels and s[i] in vowels: 
+            li[i]=0
+            print('li ele ', li)
+    li=[x for x in li if x!=0]
+    print('li ', li)
+    return "".join(li)
+    
+print(Sandwiched_Vowel('ceghij'))   #cghj
+print('a'<'z')
+
+stt = 'ABACD'
+stt1='CDABA'
+temp=stt+stt1
+q1 = []
+for i in range(len(stt)): 
+    q1.insert(0, stt[i])                      # ['A']
+    print(q1)                                 # ['B', 'A']
+                                              # ['A', 'B', 'A']
+                                              # ['C', 'A', 'B', 'A'] /n ['D', 'C', 'A', 'B', 'A'] /n ['D', 'C', 'A', 'B', 'A']
+
+print(q1)
+print(temp.count(stt1))
