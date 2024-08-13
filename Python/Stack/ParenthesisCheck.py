c6ab95d0af55f00b2ea99ee2b8aa3c23791a1d01
aky360 <95171943+aky360@users.noreply.github.com>
2024-08-13 13:35:41 +0530

    Update ParenthesisCheck.py

--- Python/Stack/ParenthesisCheck.py
+++ Python/Stack/ParenthesisCheck.py
@@ -34,3 +34,31 @@ if __name__ == "__main__":
         print("Balanced")
     else:
         print("Not Balanced")
+
+
+
+============================================================================================================
+
+
+def are_brackets_balanced(s):
+    stack = []
+    for ch in s:
+        if ch in ('(', '{', '['):
+            stack.append(ch)
+        else:
+            if stack and ((stack[-1] == '(' and ch == ')') or
+                          (stack[-1] == '{' and ch == '}') or
+                          (stack[-1] == '[' and ch == ']')):
+                stack.pop()
+            else:
+                return False
+    return not stack
+
+expr = "{()}[]"
+
+# Function call
+if are_brackets_balanced(expr):
+    print("Balanced")
+else:
+    print("Not Balanced")
+
