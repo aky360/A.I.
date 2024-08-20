ad3b3de255b81cc06c34c1dae9a8d58ffbe0791e
aky360 <95171943+aky360@users.noreply.github.com>
2024-08-21 00:19:47 +0530

    Update removeDuplicateChar.py

--- Python/Stack/removeDuplicateChar.py
+++ Python/Stack/removeDuplicateChar.py
@@ -9,7 +9,7 @@ class Solution:
         return count
         
         
-    def removeDirtyChars(self,string, mask_string):
+    def removeChars(self,string, mask_string):
         count = self.getCharCountArray(mask_string)
         actualStr_index = 0
         resultStr_inded = 0
@@ -23,14 +23,51 @@ class Solution:
                 resultStr_inded += 1
             actualStr_index += 1
         return ''.join(str_list[0:resultStr_inded])
-
-
+        
+    # OR
+        
+    def removeChars(self, string, mask_string):
+        res = ''
+        for i in string:
+            if i not in mask_string:
+                res+=i
+        return res
+        
+    # OR
+        
+    def removeChars(self,string1, string2): return ''.join([char for char in string1 if char not in set(string1)&set(string2)])
+    
+    # OR
+    
+    def removeChars(self,s1,s2):
+        res = ""
+        n1,n2=len(s1),len(s2)
+        arr = [0] * 26
+     
+        for i in range(0, n2):
+            arr[ord(s2[i]) - ord('a')] = -1 
+            
+        for i in range(0, n1):
+            if (arr[ord(s1[i]) - ord('a')] != -1):
+                res += s1[i]
+                
+        return res
+    
 if __name__ == "__main__":
 
 	post_exp = "ABC/-AK/L-*"
 	print(''.join(list(post_exp)))
-	string1 = 'computer'
-	string2 = 'cat'
+	
+	string1 = 'occurrence'
+	string2 = 'car'
+	
+	s1=set(string1)
+	print(s1)
+	s2=set(string2)
+	print(s2)
+	s=s1&s2
+	print(s)
+	
 	sol=Solution()
-	print(sol.removeDirtyChars(string1,string2))
-
+	print(sol.removeChars(string1,string2))
+	
